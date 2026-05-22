b7c67d6a98861b65e9d03cc29bc4739bc06a6361
Steven Morrison <8107863+Zaffri@users.noreply.github.com>
2026-05-23 00:27:28 +0100

    Setup web frontend scaffolding

---
+++ web-ui/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex h-screen w-screen flex-col bg-slate-50 font-sans text-slate-700 antialiased overflow-hidden">
+    <span class="text-xl">
+      Hello
+    </span>
+  </div>
+</template>
+
+<style scoped></style>
